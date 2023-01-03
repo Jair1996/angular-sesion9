5dbccaf92728061d256cb48d96b714176e0a0656
jair <jairdeyner730791@gmail.com>
2023-01-03 13:36:05 -0500

    Ruta configurada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 import { PagesModule } from './pages/pages.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes), PagesModule],
